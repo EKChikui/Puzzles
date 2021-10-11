d4a1f351a63a29c835e662474e34501c5934411d
Eduardo Chikui <ekchikui@gmail.com>
2021-10-11 19:10:07 -0300

    Create Diagonal Difference.py

---
+++ Beginner/Diagonal Difference.py
@@ -0,0 +1,41 @@
+
+# Diagonal Difference
+
+print("\n ****  Diagonal Diference  **** \n")
+
+print("Given a square matrix, calculate the absolute difference between")
+print("nthe sums of its diagonals. \n")
+
+print("For example, the square matrix arr is shown below: \n")
+
+print("  1 2 3")
+print("  4 5 6")
+print("  9 8 9 \n")
+
+print("The left-to-right diagonal = 1+5+9 = 15. The right to left")
+print("diagonal = 3+5+9 = 17. Their absolute difference is |15-17| = 2. \n")
+
+
+# Solution --------------------------------------------------------------
+
+import math
+import os
+import random
+import re
+import sys
+
+
+arr = [[11, 2, 4],
+       [4, 5, 6],
+       [10, 8, -12]]
+
+size = len(arr)
+
+for i in range(0, size):
+
+    for j in range(0, size):
+
+        
+
+
+
